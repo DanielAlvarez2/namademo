0187e17443f8d3d43f47930883371be800ee03db
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-07-28 11:25:15 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3002
+
+app.use(express.static('public'))
+
+
+app.get('/',(req, res)=>{
+    res.sendFile(index.html)
+})
+app.listen(PORT,()=>{
+    console.log(`Express Server Now Listening on port ${PORT}`);
+})
