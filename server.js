2b45575275cb406241604058b3af4f168d0c6502
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-10-31 01:00:49 -0400

    set up server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 8101
+const mongoose = require('mongoose')
+const connectDB = require('./config/database.js')
 
 app.use(express.static('public'))
 require('dotenv').config({path:'./config/.env'})
-
+connectDB()
 app.get('/',(req, res)=>{
     res.sendFile(index.html)
 })
-app.listen(PORT,()=>{
-    console.log(`Express Server Now Listening on port ${PORT}`);
+app.listen(process.env.PORT,()=>{
+    console.log(`Express Server Now Listening on port ${process.env.PORT}`);
 })
