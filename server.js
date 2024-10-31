8d0bc4434a5a0c239050aa2c0423d70a0adee6d7
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-10-31 00:52:27 -0400

    change contents of .env file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 8101
 
 app.use(express.static('public'))
-
+require('dotenv').config({path:'./config/.env'})
 
 app.get('/',(req, res)=>{
     res.sendFile(index.html)
