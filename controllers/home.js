ca0c94c9cd2f8decdbd49909a74b9bff2b4ef076
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-10-31 23:36:45 -0400

    form data arriving into MongoDB

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,24 @@
+const BTG = require('../models/BTG.js')
+
 module.exports={
     getIndex:(req,res)=>res.render('index.ejs'),
 
     getCreate:(req,res)=>res.render('create.ejs'),
 
-    postCreate:(req,res)=>{
+    postCreate:async(req,res)=>{
         console.log("req.body: ", req.body)
+
+        try{
+            await BTG.create({  section: req.body.section,
+                                bin: req.body.bin,
+                                description: req.body.description,
+                                vintage: req.body.vintage,
+                                price: req.body.price,
+                                sequence: req.body.sequence    
+            })        
+        }catch(err){
+            console.log(err)
+        }
         res.redirect('/')
     }
 }
